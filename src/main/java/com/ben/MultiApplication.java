28ca1b04163b62d3bb1d0a01dd7669e3e99f94fe
zhangchuanben <798944887@qq.com>
2016-08-13 16:33:18 +0800

    提交测试

--- src/main/java/com/ben/MultiApplication.java
+++ src/main/java/com/ben/MultiApplication.java
@@ -1,11 +1,8 @@
 package com.ben;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.ben.domain.mysql.ProductDao;
 
 @SpringBootApplication
 public class MultiApplication implements CommandLineRunner {
@@ -15,7 +12,6 @@ public class MultiApplication implements CommandLineRunner {
 	}
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println();
 	}
 	
 
